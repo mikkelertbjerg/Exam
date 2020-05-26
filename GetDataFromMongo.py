d41ce20e4188f41222fe243d9ed853aae714dd1e
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2020-05-26 21:03:28 +0200

    Update GetDataFromMongo.py

--- GetDataFromMongo.py
+++ GetDataFromMongo.py
@@ -1,79 +1,122 @@
 # -*- coding: utf-8 -*-
-
 import pymongo
 import py2neo as neo
 from py2neo import Node, Relationship
 
 #MongoDB
+#-----------------------------------------------------------------------------------------
+
 #Credentials fro connecting to the Mongodb
 myclient = pymongo.MongoClient("mongodb+srv://admin:Skole123@cluster0-wrdn4.mongodb.net/test")
 #Define which "database" we're looking into
 mydb = myclient["Exam"]
 #Define which collection we're looking into
 mycol = mydb["Cart"]
-
+#Get an object from the selected collection
 x = mycol.find_one()
 
-print(x)
-
 #Neo4j
+#-----------------------------------------------------------------------------------------
 #Credentials for connecting to the Neo4j db: <db address> <username,password>
 graph = neo.Graph("bolt://localhost:7687", auth=("neo4j", "test"))
-
 #Open the connection
 g_conn = graph.begin()
 
-#Define the node, and feed it with the information <x> fetches from mongo
-name = neo.Node("Name", name=x['name'])
-#Merge the node in the db
-g_conn.merge(name, primary_label="Name", primary_key=("name"))
-#Commit the changes
-g_conn.commit()
+#Define the nodes, and feed it with the information <x> fetches from mongo
+#Customer
+#-----------------------------------------------------------------------------------------
+customer = neo.Node("Customer",
+                #_id = x['Customer']['CustomerId'],
+                name = x['Customer']['Name'],
+                birth_date = x['Customer']['Birthdate'],
+                gender = x['Customer']['Gender'],
+                email = x['Customer']['Email'],
+                phone = x['Customer']['Telephone'])
 
+g_conn.create(customer)
 
-#SUDO CODE
-#Nodes
-customer = Node("Customer",
-                first_name = x[''],
-                last_name = x[''],
-                birth_date = x[''],
-                gender = x[''],
-                contact_info = x['']) #Look into how we define contact info in the graph.
+#Address
+#-----------------------------------------------------------------------------------------
+address = neo.Node("Address",
+                   #_id = x['Customer']['Address']['Id'],
+                   street = x['Customer']['Address']['StreetName'],
+                   no = x['Customer']['Address']['Streetno'])
 
-address = Node("Address",
-               street = x[''],
-               no = x[''])
+g_conn.create(address)
 
-city = Node("City",
-            name = x[''])
+#City
+#-----------------------------------------------------------------------------------------
+city = neo.Node("City",
+            #_id = x['Customer']['Address']['City']['Id'],
+            name = x['Customer']['Address']['City']['Name'])
 
-zipcode = Node("Zipcode",
-               code = x[''])
+g_conn.create(city)
 
-country = Node("Country",
-               name = x[''],
-               code = x[''])
+#Zipcode
+#-----------------------------------------------------------------------------------------
+zipcode = neo.Node("Zipcode",
+               #_id = x['Customer']['Address']['City']['Zipcode']['Id'],
+               code = x['Customer']['Address']['City']['Zipcode']['Code'],)
+g_conn.create(zipcode)
 
-#Look into what an order actually should contain, now that orderline is describe by the relation between order and product
-order = Node("Order",
-            status = x[''],
-            date  = x[''],
-            total = x['']) 
+#Country
+#-----------------------------------------------------------------------------------------
+country = neo.Node("Country",
+               #_id = x['Customer']['Address']['City']['Zipcode']['Country']['Id'],
+               name = x['Customer']['Address']['City']['Zipcode']['Country']['Name'],
+               code = x['Customer']['Address']['City']['Zipcode']['Country']['CountryCode'])
+g_conn.create(country)
 
-product = Node("Product",
-               name = x[''],
-               price = x[''])
+#Order
+#-----------------------------------------------------------------------------------------
+order = neo.Node("Order",
+             #_id = ['Customer']['Order']['Id'],
+             status = x['Customer']['Order']['Status'],
+             date = x['Customer']['Order']['DateCreated'],
+             total = x['Customer']['Order']['Total'])
+g_conn.create(order)
 
-category = Node ('Category',
-                 name = x[''])
+#Product(s)
+#-----------------------------------------------------------------------------------------
+for p in x['Customer']['Order']['Products']:
+    
+    quantity = p['Quantity']
+    price = p['Price']
+    
+    product = neo.Node('Product',
+                 #_id = p['Id'],
+                 name = p['Name'])
+    g_conn.create(product)
+    
+    category = neo.Node('Category',
+                        name = p['Category'])
+    g_conn.create(category)
+    
+    product_category = neo.Relationship(product, "IS A", category)
+    g_conn.create(product_category)
+    
+    order_products = neo.Relationship(order, "CONTAINS", product, quantity = quantity, price = price)
+    g_conn.create(order_products)
+    #order_products['Quantity'] = quantity
+    #order_products['Price'] = price
 
 #Relationships
-customer_address = Relationship(customer, "LIVES AT", address)
-address_city = Relationship(address, "IS IN", city)
-city_zipcode = Relationship(city, "IS IN", zipcode)
-zipcode_country = Relationship(zipcode, "IS IN", country)
-customer_orders = Relationship(customer, "PLACED", order)
-order_products = Relationship(order, "CONTAINS", product)
+#-----------------------------------------------------------------------------------------
+customer_address = neo.Relationship(customer, "LIVES AT", address)
+g_conn.create(customer_address)
 
-#Potentially smart to make graph objects?: https://py2neo.org/v4/ogm.html#labels
+address_city = neo.Relationship(address, "IS IN", city)
+g_conn.create(address_city)
 
+city_zipcode = neo.Relationship(city, "IS IN", zipcode)
+g_conn.create(city_zipcode)
+
+zipcode_country = neo.Relationship(zipcode, "IS IN", country)
+g_conn.create(zipcode_country)
+
+customer_orders = neo.Relationship(customer, "PLACED", order)
+g_conn.create(customer_orders)
+
+#Commit the changes
+#-----------------------------------------------------------------------------------------
+g_conn.commit()
