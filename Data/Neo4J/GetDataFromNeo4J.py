90058074c5adee276cc6595ba71b408e00ad7343
Fridai <Nikolaiviby3@gmail.com>
2020-05-27 20:49:06 +0200

    added loop, Zake please test

--- Data/Neo4J/GetDataFromNeo4J.py
+++ Data/Neo4J/GetDataFromNeo4J.py
@@ -6,15 +6,15 @@ import mysql.connector
 graph = neo.Graph("bolt://35-202-37-187.gcp-neo4j-sandbox.com:7687", auth=("neo4j", "cy3yxxzcXDN6UKnw"), secure=True)
 customerInfoDF = graph.run("MATCH (co:Country)<-[:`IS IN`]-(z:Zipcode)<-[:`IS IN`]-(ci:City)<-[:`IS IN`]-(a:Address)<-[:`LIVES AT`]-(c:Customer)-[:PLACED]->(o:Order) WHERE o.status = 'submitted' OR o.status = 'pending' RETURN c.first_name,c.last_name,c.birth_date, c.email, c.gender, c.phone, o.date, o.total, o.status").to_data_frame()
 
-productInfoDF = graph.run("MATCH (c:Customer)-[:PLACED]->(o:Order)-[ol:CONTAINS]->(p:Product)-[:`IS A`]->(ca:Category) RETURN p.name, ol.price, ol.quantity, ca.name").to_data_frame()
+#productInfoDF = graph.run("MATCH (c:Customer)-[:PLACED]->(o:Order)-[ol:CONTAINS]->(p:Product)-[:`IS A`]->(ca:Category) RETURN p.name, ol.price, ol.quantity, ca.name").to_data_frame()
 
 
-def postNewOrder(customer_id):
+def postNewOrder(customer_id, customer):
     connection = mysql.connector.connect(user='zakeovich_dk', password='wdfphg3mbker',
                                      host='mysql98.unoeuro.com',
                                      database='zakeovich_dk_db_cphbusiness')
     cursor = connection.cursor()
-    orderQuery = f"INSERT INTO zakeovich_dk_db_cphbusiness.order (fk_customer_id, total, date, status) VALUES ({customer_id},{customerInfoDF['o.total'][0]}, '{customerInfoDF['o.date'][0]}:00', '{customerInfoDF['o.status'][0]}');"
+    orderQuery = f"INSERT INTO zakeovich_dk_db_cphbusiness.order (fk_customer_id, total, date, status) VALUES ({customer_id},{customer['o.total'][0]}, '{customer['o.date'][0]}:00', '{customer['o.status'][0]}');"
     cursor.execute(orderQuery)
     connection.commit()
     order_id = cursor.lastrowid
@@ -24,12 +24,12 @@ def postNewOrder(customer_id):
     return order_id
 
 
-def postNewCustomer():
+def postNewCustomer(customer):
     connection = mysql.connector.connect(user='zakeovich_dk', password='wdfphg3mbker',
                                      host='mysql98.unoeuro.com',
                                      database='zakeovich_dk_db_cphbusiness')
     cursor = connection.cursor()
-    customerQuery = f"INSERT INTO zakeovich_dk_db_cphbusiness.customer (first_name, last_name, birth_date, gender) VALUES ('{customerInfoDF['c.first_name'][0]}','{customerInfoDF['c.last_name'][0]}', DATE '{customerInfoDF['c.birth_date'][0]}', '{customerInfoDF['c.gender'][0]}');"
+    customerQuery = f"INSERT INTO zakeovich_dk_db_cphbusiness.customer (first_name, last_name, birth_date, gender) VALUES ('{customer['c.first_name'][0]}','{customer['c.last_name'][0]}', DATE '{customer['c.birth_date'][0]}', '{customer['c.gender'][0]}');"
     cursor.execute(customerQuery)
     connection.commit()
     customer_id = cursor.lastrowid
@@ -38,12 +38,12 @@ def postNewCustomer():
     
     return customer_id
 
-def postNewProduct(category_id):
+def postNewProduct(category_id, product):
     connection = mysql.connector.connect(user='zakeovich_dk', password='wdfphg3mbker',
                                      host='mysql98.unoeuro.com',
                                      database='zakeovich_dk_db_cphbusiness')
     cursor = connection.cursor()
-    cursor.execute(f"SELECT p.id FROM zakeovich_dk_db_cphbusiness.product p WHERE p.name = '{productInfoDF['p.name'][0]}';")
+    cursor.execute(f"SELECT p.id FROM zakeovich_dk_db_cphbusiness.product p WHERE p.name = '{product['p.name'][0]}';")
     product_id = cursor.fetchall()
     
     if product_id[0] != None:
@@ -53,7 +53,7 @@ def postNewProduct(category_id):
         connection.close()
         return product_id[0]
     else:
-        productQuery = f"INSERT INTO zakeovich_dk_db_cphbusiness.product (name, price, fk_category_id) VALUES ('{productInfoDF['p.name'][0]}', {productInfoDF['ol.price'][0]}, {category_id});"
+        productQuery = f"INSERT INTO zakeovich_dk_db_cphbusiness.product (name, price, fk_category_id) VALUES ('{product['p.name'][0]}', {product['ol.price'][0]}, {category_id});"
         cursor.execute(productQuery)
         connection.commit()
         product_id = cursor.lastrowid
@@ -66,18 +66,18 @@ def postNewProduct(category_id):
     
    
 
-def postNewCategory():
+def postNewCategory(product):
     connection = mysql.connector.connect(user='zakeovich_dk', password='wdfphg3mbker',
                                      host='mysql98.unoeuro.com',
                                      database='zakeovich_dk_db_cphbusiness')
     cursor = connection.cursor()
-    cursor.execute(f"SELECT c.id FROM zakeovich_dk_db_cphbusiness.category c WHERE c.name = '{productInfoDF['ca.name'][0]}'")
+    cursor.execute(f"SELECT c.id FROM zakeovich_dk_db_cphbusiness.category c WHERE c.name = '{product['ca.name'][0]}'")
     category_id = cursor.fetchall()
     
     if category_id[0] != None:
         category_id = category_id[0]
     else:
-        categoryQuery = f"INSERT INTO zakeovich_dk_db_cphbusiness.category (name) VALUES ('{productInfoDF['ca.name'][0]}');"
+        categoryQuery = f"INSERT INTO zakeovich_dk_db_cphbusiness.category (name) VALUES ('{product['ca.name'][0]}');"
         cursor.execute(categoryQuery)
         connection.commit()
         category_id = cursor.lastrowid
@@ -102,10 +102,29 @@ def postOrderline(order_id, product_id):
     cursor.close()
     connection.close()
     
+x = 0
+y = 0
 
-customer_id = postNewCustomer()
-order_id = postNewOrder(customer_id)
-category_id = postNewCategory()
-product_id = postNewProduct(category_id)
 
-postOrderline(order_id, product_id)
+while x < len(customerInfoDF):
+    customer = customerInfoDF.iloc[[x]]
+    customer_id = postNewCustomer(customer)
+    order_id = postNewOrder(customer_id, customer)
+    productInfoDF = graph.run(f"MATCH (c:Customer)-[:PLACED]->(o:Order)-[ol:CONTAINS]->(p:Product)-[:`IS A`]->(ca:Category) WHERE c.email = {customer['c.email'][0]}  RETURN p.name, ol.price, ol.quantity, ca.name").to_data_frame()
+    
+    x += 1
+    while y < len(productInfoDF):
+        product = productInfoDF.iloc[[y]]
+        category_id = postNewCategory(product)
+        product_id = postNewProduct(category_id)
+        postOrderline(order_id, product_id)
+        y += 1
+        
+# =============================================================================
+# customer_id = postNewCustomer()
+# order_id = postNewOrder(customer_id)
+# category_id = postNewCategory()
+# product_id = postNewProduct(category_id)
+# 
+# postOrderline(order_id, product_id)
+# =============================================================================
