d2042f6b123e6af6965c57db92865d563814fe7e
Fridai <Nikolaiviby3@gmail.com>
2020-05-27 19:57:04 +0200

    Update RedisScript.py

--- RedisScript.py
+++ RedisScript.py
@@ -1,15 +1,24 @@
 import redis
 import py2neo as neo
 import pandas as pd
+from neo4j import GraphDatabase
+
+uri = "bolt://hobby-ppgaodfmmciegbkemkpmdcel.dbs.graphenedb.com:24787"
+user = "admin"
+passw = "b.qKKcMy6JEBOH.GdWUrSDDunj7jn6E"
+driver = GraphDatabase.driver(uri, auth=(user, passw))
+session = driver.session()
+
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-graph = neo.Graph("bolt://localhost:7687", auth=("neo4j", "Skole123"))
+graph = neo.Graph("bolt://hobby-ppgaodfmmciegbkemkpmdcel.dbs.graphenedb.com:24787", auth=("admin", "b.qKKcMy6JEBOH.GdWUrSDDunj7jn6E"), secure=True)
 
-df = graph.run("MATCH (m:Movie) RETURN m.title, m.released, m.tagline").to_data_frame()
+df = graph.run("MATCH (p:Product)-[IS_A] RETURN p.Name, p.Price, p.Category, p.").to_data_frame()
+
+
 
 df.fillna(value=pd.np.nan, inplace=True)
-        
 
 
 MoviesDict = df.to_dict('index')
